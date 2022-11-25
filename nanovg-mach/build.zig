1a03935e36c921aa851b3c988bb5ac18444e0400
dweiller <4678790+dweiller@users.noreplay.github.com>
2022-11-25 13:53:14 +1100

    Update Mach

--- nanovg-mach/build.zig
+++ nanovg-mach/build.zig
@@ -16,7 +16,7 @@ const gpu = gpu_sdk.Sdk(.{
     .gpu_dawn = gpu_dawn,
 });
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -37,7 +37,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const nanovg_demo = mach.App.init(b, .{
+    const nanovg_demo = try mach.App.init(b, .{
         .name = "nanovg-demo",
         .src = "main.zig",
         .target = target,
@@ -60,7 +60,7 @@ pub fn build(b: *std.build.Builder) void {
         },
     });
     nanovg_demo.setBuildMode(mode);
-    nanovg_demo.link(.{});
+    try nanovg_demo.link(.{});
     nanovg_demo.step.addIncludePath("../src");
     nanovg_demo.step.addCSourceFile("../src/fontstash.c", &.{ "-DFONS_NO_STDIO", "-fno-stack-protector" });
     nanovg_demo.step.addCSourceFile("../src/stb_image.c", &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" });
@@ -68,5 +68,5 @@ pub fn build(b: *std.build.Builder) void {
     nanovg_demo.install();
 
     const nanostep = b.step("run", "Run nanovg-demo");
-    nanostep.dependOn(&nanovg_demo.run().step);
+    nanostep.dependOn(try nanovg_demo.run());
 }
